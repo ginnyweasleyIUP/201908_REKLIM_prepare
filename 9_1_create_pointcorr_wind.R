2070aee257b3f9e1f20ee3bd854fcefb985c6102
ginnyweasleyIUP <jbuehler@iup.uni-heidelberg.de>
2019-09-11 18:44:38 +0200

    First Wind Plot, Revisions needed

--- 9_1_create_pointcorr_wind.R
+++ 9_1_create_pointcorr_wind.R
@@ -37,7 +37,7 @@ SIM_DATA_past1000_wind_raw <- list(
 #################################################
 
 prec <- ncdf4::nc_open("/home/ginnyweasley/Dokumente/01_Promotion/06_Daten/05_HadCM3/xnapa/xnapa_precipitation.nc")
-SIM_DATA_past1000_wind_raw$RAW$prec_raw <- as.array(ncdf4::ncvar_get(prec))
+SIM_DATA_past1000_wind_raw$RAW$prec_raw <- as.array(ncdf4::ncvar_get(prec)[,1:72,1:13790])
 ncdf4::nc_close(prec)
 remove(prec)
 
@@ -46,7 +46,7 @@ SIM_DATA_past1000_wind_raw$RAW$westerly_raw <- as.array(ncdf4::ncvar_get(wind_u)
 ncdf4::nc_close(wind_u)
 remove(wind_u)
 
-wind_v <- ncdf4::nc_open("/home/ginnyweasley/Dokumente/01_Promotion/06_Daten/05_HadCM3/xnapa/xnapa_westerly_u.nc")
+wind_v <- ncdf4::nc_open("/home/ginnyweasley/Dokumente/01_Promotion/06_Daten/05_HadCM3/xnapa/xnapa_southerly_v.nc")
 SIM_DATA_past1000_wind_raw$RAW$southerly_raw <- as.array(ncdf4::ncvar_get(wind_v))
 ncdf4::nc_close(wind_v)
 remove(wind_v)
@@ -54,6 +54,84 @@ remove(wind_v)
 #################################################
 ## Create List for all caves for pw-average #####
 #################################################
+
+#Example Lists:
+#mylist <- vector(mode = "list")
+#mylist[["CAVE1"]] = c(1,2,3,4,5)
+
+SIM_DATA_past1000_wind <- vector(mode = "list")
+
+for (ii in sites_used_short){
+  name = paste0("CAVE", ii)
+  SIM_DATA_past1000_wind[[name]] <- list(
+    WIND_MEAN_STRENGTH = array(dim = c(96,72)),
+    WIND_MEAN_STRENGTH_PREC = array(dim = c(96,72)),
+    WIND_MEAN_ANGLE = array(dim = c(96,72)),
+    WIND_MEAN_ANGLE_PREC = array(dim = c(96,72))
+  )
+  
+  for (lon in 1:96){
+    for (lat in 1:72){
+      
+      SIM_DATA_past1000_wind[[name]]$WIND_MEAN_STRENGTH[lon,lat] = mean(sqrt(SIM_DATA_past1000_wind_raw$RAW$westerly_raw[lon,lat,]^2 + 
+                                                                               SIM_DATA_past1000_wind_raw$RAW$southerly_raw[lon,lat,]^2))
+      SIM_DATA_past1000_wind[[name]]$WIND_MEAN_ANGLE[lon,lat] = mean(atan(SIM_DATA_past1000_wind_raw$RAW$southerly_raw[lon,lat,]/SIM_DATA_past1000_wind_raw$RAW$westerly_raw[lon,lat,]), na.rm = TRUE)*180/pi
+      
+      #precipitation weighted --> PREC OF THE CAVE!!!
+      lon_cave = ceiling(lon_raw/360*96)
+      lat_cave = ceiling((CAVES$site_info$latitude[ii]+90)/180*73)
+      SIM_DATA_past1000_wind[[name]]$WIND_MEAN_STRENGTH_PREC[lon,lat] = sum(SIM_DATA_past1000_wind_raw$RAW$prec_raw[lon_cave,lat_cave,] * 
+                                                                              sqrt(SIM_DATA_past1000_wind_raw$RAW$westerly_raw[lon,lat,]^2 + 
+                                                                                     SIM_DATA_past1000_wind_raw$RAW$southerly_raw[lon,lat,]^2), na.rm = TRUE) / sum(SIM_DATA_past1000_wind_raw$RAW$prec_raw[lon_cave,lat_cave,], na.rm = TRUE)
+      SIM_DATA_past1000_wind[[name]]$WIND_MEAN_ANGLE_PREC[lon,lat] = sum(SIM_DATA_past1000_wind_raw$RAW$prec_raw[lon_cave,lat_cave,]*
+                                                                           180/pi*atan(SIM_DATA_past1000_wind_raw$RAW$southerly_raw[lon,lat,]/SIM_DATA_past1000_wind_raw$RAW$westerly_raw[lon,lat,]), na.rm = TRUE) / sum(SIM_DATA_past1000_wind_raw$RAW$prec_raw[lon_cave,lat_cave,], na.rm = TRUE)
+      
+    }
+  }
+}
+
+Temp_map <-data.frame(
+  value = as.vector(SIM_DATA_past1000$GLOBAL_DATA_MEAN$GLOBAL_DATA_TEMP_MEAN),
+  x_lon = rep(c(seq(0, 179, length.out = 48),seq(-179, 0, length.out = 48)),73),
+  y_lat = rep(seq(88.5, -88.5, length.out = 73), each = 96)
+)
+
+Wind_map_27 <- data.frame(
+  radius = as.vector(SIM_DATA_past1000_wind$CAVE27$WIND_MEAN_STRENGTH),
+  angle = as.vector(SIM_DATA_past1000_wind$CAVE27$WIND_MEAN_ANGLE)/180*pi,
+  x_lon = rep(c(seq(0, 179, length.out = 48),seq(-179, 0, length.out = 48)),72),
+  y_lat = rep(seq(88.5, -88.5, length.out = 72), each = 96)
+)
+
+plot <- ggplot() +  geom_polygon(data = land_map, aes(x = long, y = lat, group = group), size = 0.05, color = "black", fill = NA, alpha = 0.5) +coord_fixed(1.3) + 
+  geom_tile(data = Temp_map, aes(x = x_lon, y = y_lat, fill = value), size = 50) + #(Non significant)
+  scale_fill_gradient2(midpoint = 0, low = "#053061", mid = "white", high = "#67001F", space = "Lab", name = "Temp") +
+  geom_spoke(data = Wind_map_27, aes(x=x_lon, y=y_lat, angle = angle, radius = radius), arrow = arrow(length = unit(.03, 'inches'))) +
+  geom_polygon(data = land_map, aes(x = long, y = lat, group = group), size = 0.05, color = "black", fill = NA, alpha = 0.5)
+
+plot
+
+ggplot(Wind_map_27,
+       aes(x = x_lon ,
+           y = y_lat,
+           fill = angle,
+           angle = angle,
+           radius = scales::rescale(radius, c(.2, .8)))) +
+  geom_raster() +
+  geom_spoke(arrow = arrow(length = unit(.05, 'inches'))) +
+  scale_fill_distiller(palette = "RdYlGn") +
+  coord_equal(expand = 0) +
+  theme(legend.position = 'bottom',
+        legend.direction = 'horizontal')
+
+#########################
+## TO DO AM DONNERSTAG
+
+# * DJF, MAM, JJA, SON - Mittel anschauen und sehen ob das anders wird
+# * vergleichen warum alles immer nach Osten geht, weil ja auf der westerly plot Karte auch viel nach Westen geht?!?
+# * dann teleconnection-wise alle significanten Punkte mit einbeziehen
+# * in STACYmaps einbauen -> radius als Option mit übergeben
+
 
 #--> Create list with assigned names for Cave sites used, where matrix of precipitation weighted average wind size is saved
   
